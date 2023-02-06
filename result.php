ac94a767549dbf5b5c3bebe294d0a158fd068906
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-06 16:26:16 +0100

    replaced word

--- result.php
+++ result.php
@@ -1,15 +1,28 @@
 <?php
 
 $paragraph = $_GET["paragraph"];
-
-echo $paragraph;
+$censored = $_GET["censored"];
 
 ?>
+
+<p>
+    <?php
+    echo $paragraph;
+    ?>
+</p>
 
 
 <p>
     La lunghezza del paragrafo è: 
     <?php
-        echo  strlen ($_GET['paragraph']);
+        echo strlen ($_GET['paragraph']);
     ?>
+    caratteri
 </p>
+
+
+<?php 
+    $censored = str_replace($censored, '***', $paragraph);
+    echo $censored;
+?>
+
